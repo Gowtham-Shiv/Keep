659562f441aec24729499d2162e467e42283c067
Gowtham Shivaram <gowthamshiv96@gmail.com>
2025-02-25 04:04:26 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import SignUp from './pages/signup/SignUp';
-
-// const routes = (
- 
-// );
 
 const App = () => {
   return (
